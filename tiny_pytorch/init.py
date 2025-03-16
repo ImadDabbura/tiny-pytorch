da4727ae8863ac5abdc3b3ed3595a8dba8762f1f
ImadDabbura <imad.dabbura@hotmail.com>
2025-03-16 17:18:01 -0500

    feat: Add xavier normal initialization

--- tiny_pytorch/init.py
+++ tiny_pytorch/init.py
@@ -92,3 +92,8 @@ def one_hot(k, n, device=None, dtype="float32", requires_grad=False):
 def xavier_uniform(fan_in, fan_out, gain=1.0, **kwargs):
     a = gain * ((6 / (fan_in + fan_out)) ** 0.5)
     return rand(fan_in, fan_out, low=-a, high=a, **kwargs)
+
+
+def xavier_normal(fan_in, fan_out, gain=1.0, **kwargs):
+    std = gain * ((2 / (fan_in + fan_out)) ** 0.5)
+    return randn(fan_in, fan_out, std=std, **kwargs)
