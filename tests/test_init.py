4d04d7416ae44202984288b860fdc0a47ad8a8f8
ImadDabbura <imad.dabbura@hotmail.com>
2025-03-16 17:18:01 -0500

    feat: Add xavier normal initialization

--- tests/test_init.py
+++ tests/test_init.py
@@ -18,3 +18,32 @@ def test_init_xavier_uniform():
         rtol=1e-4,
         atol=1e-4,
     )
+
+
+def test_init_xavier_normal():
+    np.random.seed(42)
+    np.testing.assert_allclose(
+        init.xavier_normal(3, 5, gain=0.33).numpy(),
+        np.array(
+            [
+                [
+                    0.08195783,
+                    -0.022813609,
+                    0.10686861,
+                    0.25129992,
+                    -0.038635306,
+                ],
+                [-0.038632598, 0.2605701, 0.12662673, -0.07746328, 0.08952241],
+                [
+                    -0.07646392,
+                    -0.07684541,
+                    0.039923776,
+                    -0.31569123,
+                    -0.28461143,
+                ],
+            ],
+            dtype=np.float32,
+        ),
+        rtol=1e-4,
+        atol=1e-4,
+    )
