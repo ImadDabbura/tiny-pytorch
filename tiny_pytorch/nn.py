840b2c73e37f0545594d4be3fa3c09aae8fbc688
ImadDabbura <imad.dabbura@hotmail.com>
2025-03-17 15:12:20 -0500

    feat: Add Module base class

--- tiny_pytorch/nn.py
+++ tiny_pytorch/nn.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+from . import init, ops
 from .tensor import Tensor
 
 
@@ -7,3 +10,55 @@ class Parameter(Tensor):
     so modules can be able to identify learnable parameters. All `Parameter`
     tensors have require_grad set to True.
     """
+
+
+class Module:
+    def __init__(self):
+        self._training = True
+        self._params = []
+        self._children = []
+
+    @property
+    def training(self) -> bool:
+        return self._training
+
+    @training.setter
+    def training(self, value):
+        self._training = value
+        for module in self._children:
+            module.training = value
+
+    def register_params(self, *params):
+        self._params += params
+
+    def register_modules(self, *modules):
+        self._children += modules
+
+    def parameters(self) -> list[Tensor]:
+        """Return the list of parameters in the module."""
+        return self._params + sum(
+            [module.parameters() for module in self._children], []
+        )
+
+    def children(self):
+        return self._children
+
+    def eval(self):
+        self._training = False
+        for module in self.children():
+            module.training = False
+
+    def train(self):
+        self._training = True
+        for module in self.children():
+            module.training = True
+
+    def __setattr__(self, k, v):
+        super().__setattr__(k, v)
+        if isinstance(v, Parameter):
+            self.register_params(v)
+        elif isinstance(v, Module):
+            self.register_modules(v)
+
+    def __call__(self, *args, **kwargs):
+        return self.forward(*args, **kwargs)
