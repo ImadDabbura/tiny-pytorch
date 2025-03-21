9325a76994514e827e44581f7778820327bbaa59
ImadDabbura <imad.dabbura@hotmail.com>
2025-03-21 16:39:56 -0500

    feat: Add data loader

--- tiny_pytorch/data.py
+++ tiny_pytorch/data.py
@@ -1,6 +1,10 @@
 import itertools
 import random
-from typing import Iterable, Iterator
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+from typing import Callable, Iterable, Iterator
+
+from .tensor import Tensor
 
 
 class Dataset:
@@ -75,3 +79,63 @@ class BatchSampler:
                 yield res
             if len(res) < chunk_sz:
                 return
+
+
+# FIXME: Need to figure out a way to stack xb and yb
+# Currently it returns a tuple of tensors for xb and yb
+def collate(idxs, ds):
+    xb, yb = zip(*[ds[i] for i in idxs])
+    return xb, yb
+
+
+class DataLoader:
+    """
+    A data loader that combines a dataset and a sampler to provide iterable batches of data.
+
+    The DataLoader provides an efficient way to iterate over a dataset in batches,
+    with options for shuffling, parallel data loading, and custom collation.
+
+    Args:
+        dataset (Dataset): The dataset to load data from
+        batch_size (int, optional): Number of samples per batch. Default: 1
+        n_workers (int, optional): Number of worker processes for parallel loading. Default: 1
+        shuffle (bool, optional): Whether to shuffle the data at every epoch. Default: False
+        drop_last (bool, optional): Whether to drop the last incomplete batch. Default: False
+        collate_fn (Callable, optional): Function to collate samples into batches. Default: collate
+
+    Example:
+        >>> dataset = MyDataset(...)
+        >>> loader = DataLoader(dataset, batch_size=32, shuffle=True)
+        >>> for batch_x, batch_y in loader:
+        ...     # Process batch
+    """
+
+    def __init__(
+        self,
+        dataset: Dataset,
+        batch_size: int = 1,
+        n_workers: int = 1,
+        shuffle: bool = False,
+        drop_last: bool = False,
+        collate_fn: Callable[
+            [Iterable[int], Dataset], tuple[Tensor, Tensor]
+        ] = collate,
+    ):
+
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.n_workers = n_workers
+        self.shuffle = shuffle
+        self.batch_sampler = BatchSampler(
+            Sampler(dataset, shuffle), batch_size, drop_last
+        )
+        self.collate_fn = collate_fn
+
+    def __len__(self):
+        return (len(self.dataset) - 1) // self.batch_size + 1
+
+    def __iter__(self):
+        with ProcessPoolExecutor(self.n_workers) as ex:
+            yield from ex.map(
+                partial(self.collate_fn, ds=self.dataset), self.batch_sampler
+            )
