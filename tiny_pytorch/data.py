3557900be662663b7c0a4d4950ddba3c54b0ea37
ImadDabbura <imad.dabbura@hotmail.com>
2025-03-21 12:43:42 -0500

    feat: Add NDArray Dataset

--- tiny_pytorch/data.py
+++ tiny_pytorch/data.py
@@ -19,3 +19,14 @@ class Dataset:
             for tform in self.transforms:
                 x = tform(x)
         return x
+
+
+class NDArrayDataset(Dataset):
+    def __init__(self, *arrays):
+        self.arrays = arrays
+
+    def __len__(self) -> int:
+        return self.arrays[0].shape[0]
+
+    def __getitem__(self, i) -> object:
+        return tuple([a[i] for a in self.arrays])
