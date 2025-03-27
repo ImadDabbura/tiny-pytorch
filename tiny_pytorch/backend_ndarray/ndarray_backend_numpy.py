66cf243c44bd94815b358ac7835c38b268ad9d6f
ImadDabbura <imad.dabbura@hotmail.com>
2025-03-26 19:10:53 -0500

    feat: Add from_numpy

--- tiny_pytorch/backend_ndarray/ndarray_backend_numpy.py
+++ tiny_pytorch/backend_ndarray/ndarray_backend_numpy.py
@@ -14,3 +14,14 @@ class Array:
     @property
     def size(self) -> int:
         return self.array.size
+
+
+def from_numpy(array: np.ndarray, out: Array) -> None:
+    """
+    Replace the content of `out` Array with the content of `array` numpy
+    array.
+    """
+    # Flatten returns a copy of the array
+    # This means that we're copying the content of the array into out which
+    # means the handler `array` will now point to the newly copied array
+    out.array[:] = array.flatten()
