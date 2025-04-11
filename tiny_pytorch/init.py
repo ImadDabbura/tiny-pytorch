6157672e6f5a2cbab298c0311f42e1119c1ec2fc
ImadDabbura <imad.dabbura@hotmail.com>
2025-04-10 19:20:35 -0500

    refactor: Move device logic to backend_numpy

--- tiny_pytorch/init.py
+++ tiny_pytorch/init.py
@@ -26,7 +26,8 @@ kaiming_normal
 
 from collections.abc import Iterable
 
-from .device import cpu
+import tiny_pytorch
+
 from .tensor import Tensor
 from .utils import listify
 
@@ -67,7 +68,7 @@ def rand(
     >>> rand(4, low=-1, high=1)  # 4-element tensor with values in [-1,1]
     >>> rand(2, 2, device=gpu(), dtype="float64")  # 2x2 tensor on GPU
     """
-    device = cpu() if device is None else device
+    device = tiny_pytorch.cpu() if device is None else device
     array = device.rand(*shape) * (high - low) + low
     return Tensor(
         array, device=device, dtype=dtype, requires_grad=requires_grad
@@ -110,7 +111,7 @@ def randn(
     >>> randn(4, mean=5, std=0.1)  # 4-element tensor with mean 5, std 0.1
     >>> randn(2, 2, device=gpu(), dtype="float64")  # 2x2 tensor on GPU
     """
-    device = cpu() if device is None else device
+    device = tiny_pytorch.cpu() if device is None else device
     array = device.randn(*shape) * std + mean
     return Tensor(
         array, device=device, dtype=dtype, requires_grad=requires_grad
@@ -144,7 +145,7 @@ def constant(*shape, c=1.0, device=None, dtype="float32", requires_grad=False):
     >>> constant(4, c=3.14)  # 4-element tensor filled with pi
     >>> constant(2, 2, device=gpu(), c=2.0)  # 2x2 tensor on GPU filled with 2
     """
-    device = cpu() if device is None else device
+    device = tiny_pytorch.cpu() if device is None else device
     array = device.ones(shape, dtype=dtype) * c  # note: can change dtype
     return Tensor(
         array, device=device, dtype=dtype, requires_grad=requires_grad
@@ -239,7 +240,7 @@ def randb(*shape, p=0.5, device=None, dtype="bool", requires_grad=False):
     >>> randb(4, p=0.8)  # 4-element tensor, 80% chance of 1s
     >>> randb(2, 2, device=gpu())  # 2x2 binary tensor on GPU
     """
-    device = cpu() if device is None else device
+    device = tiny_pytorch.cpu() if device is None else device
     array = device.rand(*shape) <= p
     return Tensor(
         array, device=device, dtype=dtype, requires_grad=requires_grad
@@ -275,7 +276,7 @@ def one_hot(k, n, device=None, dtype="float32", requires_grad=False):
     >>> one_hot(2, 5, device=gpu())  # 5x2 one-hot tensor on GPU
     """
     n = listify(n) if isinstance(n, Iterable) else n
-    device = cpu() if device is None else device
+    device = tiny_pytorch.cpu() if device is None else device
     return Tensor(
         device.one_hot(k, n, dtype=dtype),
         device=device,
