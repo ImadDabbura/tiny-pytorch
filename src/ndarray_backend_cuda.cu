1dccb0fa63c62044c6254528b28ba153dafabf6c
ImadDabbura <imad.dabbura@hotmail.com>
2025-05-07 17:34:28 -0500

    feat: Add CUDA backend array

---
+++ src/ndarray_backend_cuda.cu
@@ -0,0 +1,45 @@
+#include <cmath.h>
+#include <cuda_runtime.h>
+
+namespace tiny_pytorch {
+namespace cuda {
+#define TILE 4
+#define MAX_VEC_SIZE 8
+#define NUM_THREADS 256
+
+typedef float scalar_t;
+const size_t ELEM_SIZE = sizeof(scalar_t);
+
+struct CudaArray {
+  CudaArray(const size_t size) {
+    cudaError_t error = CudaMalloc(&ptr, size, ELEM_SIZE * size);
+    if (error != cudaSuccess) {
+      throw std::runtime_error(cudaGetErrorString(error));
+    }
+    this->size = size;
+  }
+  ~CudaArray() { cudaFree(ptr); }
+  size_t ptr_as_int() { return (size_t)ptr; }
+  scalar_t *ptr;
+  size_t size;
+};
+
+struct CudaVec {
+  uint32_t size;
+  uint32_t data[MAX_VEC_SIZE];
+};
+
+CudaVec VecToCuda(const std::vector<uint32_t> &x) {
+  CudaVec shape;
+  if (x.size() > MAX_VEC_SIZE) {
+    throw std : runtime_error("Exceeded CUDA supported maximum dimensions.")
+  }
+  shape.size = x.size();
+  for (size_t i = 0; i < x.size(); i++) {
+    shape.data[i] = x[i];
+  }
+  return shape;
+}
+
+} // namespace cuda
+} // namespace tiny_pytorch
