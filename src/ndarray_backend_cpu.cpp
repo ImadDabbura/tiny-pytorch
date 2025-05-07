deaf0d416decd9f3279acab068e2ea532fb6e6c8
ImadDabbura <imad.dabbura@hotmail.com>
2025-05-06 19:58:48 -0500

    feat: Add CPU backend pybind11

--- src/ndarray_backend_cpu.cpp
+++ src/ndarray_backend_cpu.cpp
@@ -1,6 +1,9 @@
 #include <cmath>
 #include <cstdint>
 #include <iostream>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 namespace tiny_pytorch {
 namespace cpu {
@@ -367,3 +370,60 @@ void MatmulTiled(const AlignedArray &a, const AlignedArray &b,
 
 } // namespace cpu
 } // namespace tiny_pytorch
+
+PYBIND11_MODULE(ndarray_backend_cpu, m) {
+  namespace py = pybind11;
+  using namespace tiny_pytorch;
+  using namespace cpu;
+
+  m.attr("__device_name__") = "cpu";
+  m.attr("__tile_size__") = TILE;
+
+  py::class_<AlignedArray>(m, "Array")
+      .def(py::init<size_t>(), py::return_value_policy::take_ownership)
+      .def("ptr", &AlignedArray::ptr_as_int)
+      .def_readonly("size", &AlignedArray::size);
+
+  // return numpy array (with copying for simplicity, otherwise garbage
+  // collection is a pain)
+  m.def("to_numpy", [](const AlignedArray &a, std::vector<size_t> shape,
+                       std::vector<size_t> strides, size_t offset) {
+    std::vector<size_t> numpy_strides = strides;
+    std::transform(numpy_strides.begin(), numpy_strides.end(),
+                   numpy_strides.begin(),
+                   [](size_t &c) { return c * ELEM_SIZE; });
+    return py::array_t<scalar_t>(shape, numpy_strides, a.ptr + offset);
+  });
+
+  // convert from numpy (with copying)
+  m.def("from_numpy", [](py::array_t<scalar_t> a, AlignedArray *out) {
+    std::memcpy(out->ptr, a.request().ptr, out->size * ELEM_SIZE);
+  });
+  m.def("ewise_setitem", EwiseSetitem);
+  m.def("scalar_setitem", ScalarSetitem);
+  m.def("ewise_add", EwiseAdd);
+  m.def("scalar_add", ScalarAdd);
+
+  m.def("ewise_mul", EwiseMul);
+  m.def("scalar_mul", ScalarMul);
+  m.def("ewise_div", EwiseDiv);
+  m.def("scalar_div", ScalarDiv);
+  m.def("scalar_power", ScalarPower);
+
+  m.def("ewise_maximum", EwiseMaximum);
+  m.def("scalar_maximum", ScalarMaximum);
+  m.def("ewise_eq", EwiseEq);
+  m.def("scalar_eq", ScalarEq);
+  m.def("ewise_ge", EwiseGe);
+  m.def("scalar_ge", ScalarGe);
+
+  m.def("ewise_log", EwiseLog);
+  m.def("ewise_exp", EwiseExp);
+  m.def("ewise_tanh", EwiseTanh);
+
+  m.def("matmul", Matmul);
+  m.def("matmul_tiled", MatmulTiled);
+
+  m.def("reduce_max", ReduceMax);
+  m.def("reduce_sum", ReduceSum);
+}
