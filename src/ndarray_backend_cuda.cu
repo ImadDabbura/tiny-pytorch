76a2a2422af9566d3da131ff7a583a9ad8354d53
ImadDabbura <imad.dabbura@hotmail.com>
2025-05-07 17:50:46 -0500

    feat: Add CUDA backend ewise greater_eq

--- src/ndarray_backend_cuda.cu
+++ src/ndarray_backend_cuda.cu
@@ -196,5 +196,18 @@ void ScalarEq(const CudaArray &a, scalar_t val, CudaArray *out) {
       a.ptr, val, out->ptr, out->size);
 }
 
+__global__ void EwiseGeKernel(const scalar_t *a, const scalar_t *b,
+                              scalar_t *out, size_t n) {
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+  if (i < n) {
+    out[i] = a[i] >= b[i];
+  }
+}
+
+void EwiseGe(const CudaArray &a, const CudaArray &b, CudaArray *out) {
+  EwiseGeKernel<<<ceil(out->size, NUM_THREADS), NUM_THREADS>>>(
+      a.ptr, b.ptr, out->ptr, out->size);
+}
+
 } // namespace cuda
 } // namespace tiny_pytorch
