62e127ddac76f5ca0ea692a514c94f0013678c76
ImadDabbura <imad.dabbura@hotmail.com>
2025-05-07 17:39:51 -0500

    feat: Add CUDA backend scalar add

--- src/ndarray_backend_cuda.cu
+++ src/ndarray_backend_cuda.cu
@@ -66,5 +66,18 @@ void EwiseAdd(const CudaArray &a, const CudaArray &b, CudaArray *out) {
       a.ptr, b.ptr, out->ptr, out->size);
 }
 
+__global__ void ScalarAddKernel(const scalar_t *a, scalar_t val, scalar_t *out,
+                                size_t n) {
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+  if (i < n) {
+    out[i] = a[i] + val;
+  }
+}
+
+void ScalarAdd(const CudaArray &a, scalar_t val, CudaArray *out) {
+  ScalarAddKernel<<<ceil(out->size, NUM_THREADS), NUM_THREADS>>>(
+      a.ptr, val, out->ptr, out->size);
+}
+
 } // namespace cuda
 } // namespace tiny_pytorch
