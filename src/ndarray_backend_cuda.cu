28182c7dc9ad8013152bbaa74f77b20608e5e678
ImadDabbura <imad.dabbura@hotmail.com>
2025-05-07 17:48:59 -0500

    feat: Add CUDA backend ewise equality

--- src/ndarray_backend_cuda.cu
+++ src/ndarray_backend_cuda.cu
@@ -170,5 +170,18 @@ void ScalarMaximum(const CudaArray &a, scalar_t val, CudaArray *out) {
       a.ptr, val, out->ptr, out->size);
 }
 
+__global__ void EwiseEqKernel(const scalar_t *a, const scalar_t *b,
+                              scalar_t *out, size_t n) {
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+  if (i < n) {
+    out[i] = a[i] == b[i];
+  }
+}
+
+void EwiseEq(const CudaArray &a, const CudaArray &b, CudaArray *out) {
+  EwiseEqKernel<<<ceil(out->size, NUM_THREADS), NUM_THREADS>>>(
+      a.ptr, b.ptr, out->ptr, out->size);
+}
+
 } // namespace cuda
 } // namespace tiny_pytorch
